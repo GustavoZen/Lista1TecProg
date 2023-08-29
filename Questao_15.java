6920763991989f23a59111d12b1e94b2ddd42c2d
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-08-29 13:40:28 -0300

    Create Questao_15.java

---
+++ Questao_15.java
@@ -0,0 +1,12 @@
+package lista_01;
+
+public class Questão_15 {
+	public static void main(String[] args) {
+	  int A=15,B=5;
+	  if(A<10)
+      System.out.println("A == 10");
+    if(A+B==20)
+      System.out.println("A + B == 20");
+    if(!A<10 && !A+B==20)
+      System.out.println("número não valido");
+  }
