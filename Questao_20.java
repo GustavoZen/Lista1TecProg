3ea0fc2ed842c418b97e1a5bd5513842f41fe4f4
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:53:45 -0300

    Update Questao_20.java

--- Questao_20.java
+++ Questao_20.java
@@ -2,12 +2,12 @@ package lista_01;
 
 public class Questao_20 {
 	public static void main(String[] args) {
-	  int A=15,B=5;
-	  if(A>10 || A + B == 20)
-            System.out.println("números válidos");
-    else
-	          System.out.println("número não válido");
-    if(true)
-            System.out.println("Sejam bem-vindos à disciplina técnicas de programação")
+		int A = 15, B = 5;
+		if (A > 10 || A + B == 20)
+			System.out.println("números válidos");
+		else
+			System.out.println("número não válido");
+		if (true)
+			System.out.println("Sejam bem-vindos à disciplina técnicas de programação");
 	}
 }
