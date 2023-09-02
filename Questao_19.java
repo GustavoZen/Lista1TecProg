ef3c119183c093f148e22d2b3910a45ad682f838
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:29:14 -0300

    Create Questao_19.java

---
+++ Questao_19.java
@@ -0,0 +1,15 @@
+package lista_01;
+
+public class Questao_19 {
+	public static void main(String[] args) {
+	  int A=15,B=5;
+	  if(A>10)
+            System.out.println("A > 10");
+    else
+	          System.out.println("A <= 10");
+    if(A+B==20)
+              System.out.println("A+B == 20");
+    else
+              System.out.println("A + B != 20");
+	}
+}
