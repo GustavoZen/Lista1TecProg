58da5473043b3778472a6a315a801640cbfa5081
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:27:55 -0300

    Create Questao _17.java

---
+++ Questao _17.java
@@ -0,0 +1,15 @@
+package lista_01;
+
+public class Questao_17{
+	public static void main(String[] args) {
+	  int A=15,B=5;
+	  if(A>10 || A+B==20)
+            System.out.println("número válido");
+    else if(A==B)
+            System.out.println("A é igual a B");
+    else if(A != 10 && B != 10 && A<10)
+            System.out.println("A é menor que 10")
+    else
+            System.out.println("número não válido")
+	}
+}
