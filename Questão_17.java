48819e15a09ec23a012acc213d92144ff09a0766
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:49:47 -0300

    Update and rename Questao _17.java to Questão_17.java

--- Questão_17.java
+++ Questão_17.java
@@ -8,8 +8,8 @@ public class Questao_17{
     else if(A==B)
             System.out.println("A é igual a B");
     else if(A != 10 && B != 10 && A<10)
-            System.out.println("A é menor que 10")
+            System.out.println("A é menor que 10");
     else
-            System.out.println("número não válido")
+            System.out.println("número não válido");
 	}
 }
