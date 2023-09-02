9e24044c446ab851d55aff5080c794bd686f9cde
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:45:21 -0300

    Update Questao_14.java

--- Questao_14.java
+++ Questao_14.java
@@ -1,15 +1,14 @@
 package lista_01;
 
-public class Questao_14 {
+public class Questão_14 {
 	public static void main(String[] args) {
-	  int A=15,B=5;
-	  if(A>10) {
-            System.out.println("A > 10");
-            if(A+B==20) {
-	      System.out.println("A + B == 20");
-            }
-            else
-              System.out.println("número não valido");
-          }
+		int A = 15, B = 5;
+		if (A > 10) {
+			System.out.println("A > 10");
+			if (A + B == 20) {
+				System.out.println("A + B == 20");
+			} else
+				System.out.println("número não valido");
+		}
 	}
 }
