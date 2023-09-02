39819362ee1fa859b2aa671e6cf9604d205c694b
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:46:15 -0300

    Update Questao_15.java

--- Questao_15.java
+++ Questao_15.java
@@ -1,12 +1,13 @@
-package lista_01;
+package Lista_01;
 
 public class Questao_15 {
 	public static void main(String[] args) {
-	  int A=15,B=5;
-	  if(A<10)
-      System.out.println("A == 10");
-    if(A+B==20)
-      System.out.println("A + B == 20");
-    if(!A<10 && !A+B==20)
-      System.out.println("número não valido");
-  }
+		int A = 15, B = 5;
+		if (A < 10)
+			System.out.println("A == 10");
+		if (A + B == 20)
+			System.out.println("A + B == 20");
+		if (!(A < 10) && !(A + B == 20))
+			System.out.println("número não valido");
+	}
+}
