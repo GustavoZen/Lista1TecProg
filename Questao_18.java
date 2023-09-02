8e9c5a31c894a340c611ad1e9454e5819595eca8
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-09-02 17:28:27 -0300

    Create Questao_18.java

---
+++ Questao_18.java
@@ -0,0 +1,11 @@
+package lista_01;
+
+public class Questao_18 {
+	public static void main(String[] args) {
+	  int A=15,B=5;
+	  if(A>10 || A+B == 20)
+            System.out.println("número válido");
+    else
+	          System.out.println("número não válido")
+	}
+}
